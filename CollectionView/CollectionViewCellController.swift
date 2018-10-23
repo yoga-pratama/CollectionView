143befd351e16268604b696fcbed113ebf0c0cd4
yoga <yogamadness@gmail.com>
2018-10-23 08:48:31 +0700

    Adding New File For CollectionView

---
+++ CollectionView/CollectionViewCellController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellController.swift
+//  CollectionView
+//
+//  Created by Yoga Pratama on 23/10/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellController: UICollectionViewCell {
+    
+}
